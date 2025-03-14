dd8a2f5d02ac255555b77990f970646e452e40cc
蘇銘凱 <sjs47311@gmail.com>
2025-03-14 13:36:53 +0800

    refactor: 更新 auth-service 的 Dockerfile，使用多階段構建並優化編譯過程；新增從環境變量獲取 Firebase 憑證的功能

--- auth-service/internal/infrastructure/firebase/firebase.go
+++ auth-service/internal/infrastructure/firebase/firebase.go
@@ -2,7 +2,9 @@ package firebase
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/auth"
@@ -46,4 +48,39 @@ func InitFirebase(ctx context.Context, credFile, projectID string) (*Firebase, e
 		Auth:     auth,
 		Database: database,
 	}, nil
+}
+
+func InitializeApp() (*firebase.App, error) {
+	// 嘗試從環境變量獲取憑證
+	credentials := os.Getenv("FIREBASE_CREDENTIALS")
+	if credentials != "" {
+		// 使用環境變量中的憑證
+		conf := &firebase.Config{
+			DatabaseURL: "https://order-manager-system-a6931-default-rtdb.firebaseio.com",
+		}
+
+		opt := option.WithCredentialsJSON([]byte(credentials))
+		app, err := firebase.NewApp(context.Background(), conf, opt)
+		if err != nil {
+			return nil, fmt.Errorf("error initializing app with credentials JSON: %v", err)
+		}
+		return app, nil
+	}
+
+	// 如果環境變量不存在，回退到使用憑證文件
+	credentialsFile := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+	if credentialsFile == "" {
+		credentialsFile = "firebase-credentials.json"
+	}
+
+	conf := &firebase.Config{
+		DatabaseURL: "https://order-manager-system-a6931-default-rtdb.firebaseio.com",
+	}
+
+	app, err := firebase.NewApp(context.Background(), conf)
+	if err != nil {
+		return nil, fmt.Errorf("error initializing app: %v", err)
+	}
+
+	return app, nil
 }
